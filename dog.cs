ceed27276e7d9998410617a96f1066ed215dfb81
ahdp861 <lkssaa862@gmail.com>
2024-12-25 14:08:54 +0500

    Create dog.cs

---
+++ dog.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+using System.Runtime.Intrinsics.X86;
+using System.Xml.Linq;
+namespace aaa{
+class Cat : IMeow
+{
+    private string name;
+    
+    public Cat(string name1) {name = name1;}
+
+    public override string ToString()
+    {
+        return $"кот: {name}";
+    }
+
+    public void meow(int n = 1)
+    {
+        Console.Write($"{name}: мяу");
+        for (int i = 0; i < n-1; i++) { Console.Write($"-мяу"); }
+        Console.Write("!\n");
+    }
+}}
